82dd8728bd554b6e218bbcb9137898cf7cf46d99
Khizra Ghaffar <135971161+khizraghaffarkk@users.noreply.github.com>
2025-06-29 22:19:33 +0300

    Update config.h

--- include/config.h
+++ include/config.h
@@ -2,6 +2,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define WEATHER_API_KEY "4fd5f2a6049d90813f2b39ace4c13b81"  // Replace with your actual OpenWeatherMap API key
+#define WEATHER_API_KEY "{YOUR_API_KEY}"  // Replace with your actual OpenWeatherMap API key
 
 #endif
